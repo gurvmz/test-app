11b0a43497db2749283f28631a6642d01c3298a7
Gurvinder <gurvmz@amazon.com>
2020-11-03 15:22:26 -0500

    corrected host

--- runserver.py
+++ runserver.py
@@ -6,7 +6,7 @@ from os import environ
 from python_webapp_flask import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
